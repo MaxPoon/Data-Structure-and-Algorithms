5435f26e6157dcd1ca82a28f810bcea442344391
maxpoon <maxpanziyuan@gmail.com>
2016-12-04 01:54:48 +0800

    update codes

---
+++ Algorithmic-Toolbox/week3/assignment/change/Change.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Change {
+    private static int getChange(int m) {
+        //write your code here
+        int n = m/10;
+        m = m % 10;
+        n += m/5;
+        m = m % 5;
+        n += m;
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        System.out.println(getChange(m));
+
+    }
+}
+
---
+++ Algorithmic-Toolbox/week3/assignment/fractional_knapsack/FractionalKnapsack.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class FractionalKnapsack {
+	private static double getOptimalValue(int capacity, int[] values, int[] weights) {
+		double value = 0;
+		//write your code here
+		ArrayList<Item> items = new ArrayList<Item>();
+		for(int i=0; i<values.length; i++){
+			Item item = new Item(values[i], weights[i]);
+			items.add(item);
+		}
+		Collections.sort(items, Collections.reverseOrder());
+		Iterator<Item> it = items.iterator();
+		while(capacity > 0 && it.hasNext()){
+			Item item = it.next();
+			if (capacity > item.weight){
+				capacity -= item.weight;
+				value += item.value;
+			}else{
+				value += capacity*item.unitPrice;
+				capacity = 0;
+			}
+		}
+		return value;
+	}
+
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int capacity = scanner.nextInt();
+		int[] values = new int[n];
+		int[] weights = new int[n];
+		for (int i = 0; i < n; i++) {
+			values[i] = scanner.nextInt();
+			weights[i] = scanner.nextInt();
+		}
+		System.out.println(getOptimalValue(capacity, values, weights));
+	}
+} 
+
+class Item implements Comparable<Item>{
+	public int weight, value;
+	public double unitPrice;
+	Item(int value, int weight){
+		this.value = value;
+		this.weight = weight;
+		unitPrice = (double)value/weight;
+	}
+
+	public int compareTo(Item other){
+		return (int)(this.unitPrice - other.unitPrice);
+	}
+}

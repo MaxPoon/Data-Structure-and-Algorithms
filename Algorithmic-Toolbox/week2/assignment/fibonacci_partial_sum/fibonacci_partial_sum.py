c6188c51e20daae26f98c733e818bea80fb7c70c
maxpoon <maxpanziyuan@gmail.com>
2016-12-04 00:55:37 +0800

    finish week2

--- Algorithmic-Toolbox/week2/assignment/fibonacci_partial_sum/fibonacci_partial_sum.py
+++ Algorithmic-Toolbox/week2/assignment/fibonacci_partial_sum/fibonacci_partial_sum.py
@@ -1,27 +1,21 @@
 # Uses python3
 import sys
 
-def fibonacci_partial_sum_naive(from_, to):
-    if to <= 1:
-        return to
+def Huge_Fib(n,m):
 
-    previous = 0
-    current  = 1
-
-    for _ in range(from_ - 1):
-        previous, current = current % 10, (previous + current) % 10
-
-    sum = current
-
-    for _ in range(to - from_):
-        previous, current = current % 10, (previous + current) %10
-        sum += current
-        sum %= 10
-
-    return sum % 10
+    # Initialize a matrix [[1,1],[1,0]]    
+    v1, v2, v3 = 1, 1, 0  
+    # Perform fast exponentiation of the matrix (quickly raise it to the nth power)
+    for rec in bin(n)[3:]:
+        calc = (v2*v2) % m
+        v1, v2, v3 = (v1*v1+calc) % m, ((v1+v3)*v2) % m, (calc+v3*v3) % m
+        if rec == '1': v1, v2, v3 = (v1+v2) % m, v1, v2
+    return v2
 
 
 if __name__ == '__main__':
     input = sys.stdin.read();
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    ans = Huge_Fib(to+2, 10) - Huge_Fib(from_+1, 10)
+    ans += 10 if ans < 0 else 0
+    print(ans)
--- Algorithmic-Toolbox/week2/assignment/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
+++ Algorithmic-Toolbox/week2/assignment/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
@@ -1,21 +1,18 @@
 # Uses python3
 import sys
 
-def fibonacci_sum_naive(n):
-    if n <= 1:
-        return n
+def Huge_Fib(n,m):
 
-    previous = 0
-    current  = 1
-    sum      = 1
-
-    for _ in range(n - 1):
-        previous, current = current % 10, (previous + current)%10
-        sum += current
-
-    return sum % 10
+	# Initialize a matrix [[1,1],[1,0]]    
+	v1, v2, v3 = 1, 1, 0  
+	# Perform fast exponentiation of the matrix (quickly raise it to the nth power)
+	for rec in bin(n)[3:]:
+		calc = (v2*v2) % m
+		v1, v2, v3 = (v1*v1+calc) % m, ((v1+v3)*v2) % m, (calc+v3*v3) % m
+		if rec == '1': v1, v2, v3 = (v1+v2) % m, v1, v2
+	return v2
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(fibonacci_sum_naive(n))
+	input = sys.stdin.read()
+	n = int(input)
+	print((Huge_Fib(n+2,10)-1)%10)

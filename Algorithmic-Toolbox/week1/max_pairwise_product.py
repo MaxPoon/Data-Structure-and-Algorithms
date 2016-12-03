fafca2bf2db91c39217a29588279bbf0c37ac077
maxpoon <maxpanziyuan@gmail.com>
2016-12-03 20:43:49 +0800

    finish week1

---
+++ Algorithmic-Toolbox/week1/max_pairwise_product.py
@@ -0,0 +1,18 @@
+# Uses python3
+n = int(input())
+a = [int(x) for x in input().split()]
+assert(len(a) == n)
+
+# result = 0
+
+# for i in range(0, n):
+# 	for j in range(i+1, n):
+# 		if a[i]*a[j] > result:
+# 			result = a[i]*a[j]
+
+# print(result)
+
+m1 = max(a)
+a.remove(m1)
+m2 = max(a)
+print(m1*m2)

6175b34a743e6817f1fab0e9d88ff4006418e084
Max Poon <maxpanziyuan@gmail.com>
2016-12-08 01:34:51 +0800

    finish week3

--- Data-Structures/week3/assignment/merging_tables/merging_tables.py
+++ Data-Structures/week3/assignment/merging_tables/merging_tables.py
@@ -9,23 +9,34 @@ parent = list(range(0, n))
 ans = max(lines)
 
 def getParent(table):
-    # find parent and compress path
-    return parent[table]
+	# find parent and compress path
+	return parent[table]
 
 def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
+	global ans
+	realDestination, realSource = getParent(destination), getParent(source)
 
-    if realDestination == realSource:
-        return False
+	if realDestination == realSource:
+		return False
 
-    # merge two components
-    # use union by rank heuristic 
-    # update ans with the new maximum table size
-    
-    return True
+	# merge two components
+	if rank[realDestination] < rank[realSource]:
+		parent[realDestination] = realSource
+		lines[realSource] += lines[realDestination]
+		ans = max(ans, lines[realSource])
+	else:
+		parent[realSource] = realDestination
+		lines[realDestination] += lines[realSource]
+		ans = max(ans, lines[realDestination])
+		if rank[realDestination] == rank[realSource]:
+			rank[realDestination] += 1
+	# use union by rank heuristic 
+	# update ans with the new maximum table size
+	
+	return True
 
 for i in range(m):
-    destination, source = map(int, sys.stdin.readline().split())
-    merge(destination - 1, source - 1)
-    print(ans)
-    
+	destination, source = map(int, sys.stdin.readline().split())
+	merge(destination - 1, source - 1)
+	print(ans)
+	

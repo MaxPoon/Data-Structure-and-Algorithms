62dfd484ebf11a5f600caddae20158b9f5eddd09
Max Poon <maxpanziyuan@gmail.com>
2016-12-08 23:41:07 +0800

    finish course

--- Data-Structures/week4/assignment/hash_substring/hash_substring.py
+++ Data-Structures/week4/assignment/hash_substring/hash_substring.py
@@ -10,18 +10,12 @@ def print_occurrences(output):
 def get_occurrences(pattern, text):
 	output = []
 	l = len(pattern)
-	hashedValue = 0
-	currentHashed = 0
-	for i,c in enumerate(pattern):
-		a = ord(c)
-		hashedValue += (a*(x**i)) % p
-	for i,c in enumerate(text[:l]):
-		a = ord(c)
-		currentHashed += (a*(x**i)) % p
+	hashedValue = sum([ord(pattern[i]) for i in range(len(pattern))])
+	currentHashed = sum([ord(text[:l][i]) for i in range(len(text[:l]))])
 	if text[:l] == pattern:
 		output.append(0)
 	for i in range(1,len(text)-l+1):
-		currentHashed = ((currentHashed - ord(text[i-1]))//x + ord(text[i+l-1])*(x**(l-1))) % p
+		currentHashed = currentHashed - ord(text[i-1]) + ord(text[i+l-1])
 		if currentHashed == hashedValue:
 			if text[i:i+l] == pattern: output.append(i)
 	return output

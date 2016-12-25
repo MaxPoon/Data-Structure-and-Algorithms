42ab970b557b88a60e83351880501f852cd6bb46
Max Poon <maxpanziyuan@gmail.com>
2016-12-25 09:05:13 +0800

    start week3

---
+++ Algorithms-on-Graph/week3/assignment/bfs/bfs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using std::vector;
+using std::queue;
+
+int distance(vector<vector<int> > &adj, int s, int t) {
+  //write your code here
+  return -1;
+}
+
+int main() {
+  int n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (int i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+    adj[y - 1].push_back(x - 1);
+  }
+  int s, t;
+  std::cin >> s >> t;
+  s--, t--;
+  std::cout << distance(adj, s, t);
+}
---
+++ Algorithms-on-Graph/week3/assignment/bipartite/Bipartite.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Bipartite {
+    private static int bipartite(ArrayList<Integer>[] adj) {
+        //write your code here
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            adj[x - 1].add(y - 1);
+            adj[y - 1].add(x - 1);
+        }
+        System.out.println(bipartite(adj));
+    }
+}
+

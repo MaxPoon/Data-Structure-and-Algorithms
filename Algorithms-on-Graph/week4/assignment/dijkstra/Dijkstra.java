9d1ce96cfcebcc580fbbf4950a8312b2eee91944
Max Poon <maxpanziyuan@gmail.com>
2016-12-25 21:52:24 +0800

    pass week4

--- Algorithms-on-Graph/week4/assignment/dijkstra/Dijkstra.java
+++ Algorithms-on-Graph/week4/assignment/dijkstra/Dijkstra.java
@@ -1,31 +1,57 @@
 import java.util.*;
 
 public class Dijkstra {
-    private static int distance(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, int t) {
-        return -1;
-    }
+	private static int distance(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, int t) {
+		PriorityQueue<Node> q = new PriorityQueue<Node>(adj.length, (a,b) -> a.distance - b.distance);
+		Node start = new Node(s, 0);
+		q.offer(start);
+		int[] dist = new int[adj.length];
+		for(int i=0; i<adj.length; i++) dist[i] = -1;
+		dist[s] = 0;
+		while(!q.isEmpty()){
+			Node x = q.poll();
+			if(x.number == t) return x.distance;
+			Iterator<Integer> costIt = cost[x.number].iterator();
+			for(int y: adj[x.number]){
+				int d = costIt.next();
+				if(dist[y]==-1 || dist[y]>dist[x.number]+d){
+					dist[y] = dist[x.number] + d;
+					Node newNode = new Node(y, dist[y]);
+					q.offer(newNode);
+				}
+			}
+		}
+		return -1;
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-            cost[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < m; i++) {
-            int x, y, w;
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            w = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            cost[x - 1].add(w);
-        }
-        int x = scanner.nextInt() - 1;
-        int y = scanner.nextInt() - 1;
-        System.out.println(distance(adj, cost, x, y));
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+		ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			adj[i] = new ArrayList<Integer>();
+			cost[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < m; i++) {
+			int x, y, w;
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			w = scanner.nextInt();
+			adj[x - 1].add(y - 1);
+			cost[x - 1].add(w);
+		}
+		int x = scanner.nextInt() - 1;
+		int y = scanner.nextInt() - 1;
+		System.out.println(distance(adj, cost, x, y));
+	}
 }
 
+class Node{
+	public int distance, number;
+	Node(int number, int distance){
+		this.number = number;
+		this.distance = distance;
+	}
+}
--- Algorithms-on-Graph/week4/assignment/negative_cycle/NegativeCycle.java
+++ Algorithms-on-Graph/week4/assignment/negative_cycle/NegativeCycle.java
@@ -1,31 +1,52 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class NegativeCycle {
-    private static int negativeCycle(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost) {
-        // write your code here
-        return 0;
-    }
+	private static int negativeCycle(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost) {
+		int n = adj.length-1;
+		int[] dist = new int[n+1];
+		for(int i=0; i<n; i++) dist[i] = 99999;
+		dist[n] = 0;
+		for(int i=0; i<n; i++){
+			boolean changed = false;
+			for(int x=0;x<n+1;x++){
+				Iterator<Integer> costIt = cost[x].iterator();
+				for(int y: adj[x]){
+					int d = costIt.next();
+					if(dist[y]>dist[x]+d){
+						changed = true;
+						dist[y] = dist[x] + d;
+					}
+				}
+			}
+			if(!changed) return 0;
+			if(changed && i==n-1) return 1;
+		}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-            cost[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < m; i++) {
-            int x, y, w;
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            w = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            cost[x - 1].add(w);
-        }
-        System.out.println(negativeCycle(adj, cost));
-    }
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n+1];
+		ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n+1];
+		for (int i = 0; i < n+1; i++) {
+			adj[i] = new ArrayList<Integer>();
+			cost[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < m; i++) {
+			int x, y, w;
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			w = scanner.nextInt();
+			adj[x - 1].add(y - 1);
+			cost[x - 1].add(w);
+		}
+		for (int i=0; i<n; i++){
+			adj[n].add(i);
+			cost[n].add(0);
+		}
+		System.out.println(negativeCycle(adj, cost));
+	}
 }
-

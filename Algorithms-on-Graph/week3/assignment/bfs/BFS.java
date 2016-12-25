a7b41c64f34862f109f43608480b560b5c822d2c
Max Poon <maxpanziyuan@gmail.com>
2016-12-25 09:57:27 +0800

    finish week3

--- Algorithms-on-Graph/week3/assignment/bfs/BFS.java
+++ Algorithms-on-Graph/week3/assignment/bfs/BFS.java
@@ -4,29 +4,43 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BFS {
-    private static int distance(ArrayList<Integer>[] adj, int s, int t) {
-        //write your code here
-        return -1;
-    }
+	private static int distance(ArrayList<Integer>[] adj, int s, int t) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		int[] dist = new int[adj.length];
+		for(int i=0; i<adj.length; i++) dist[i] = -1;
+		q.add(s);
+		dist[s] = 0;
+		while(!q.isEmpty()){
+			int x = q.poll();
+			for(int y: adj[x]){
+				if (dist[y]==-1) {
+					dist[y]=dist[x]+1;
+					if(y==t) return dist[y];
+					q.add(y);
+				}
+			}
+		}
+		return -1;
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < m; i++) {
-            int x, y;
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            adj[y - 1].add(x - 1);
-        }
-        int x = scanner.nextInt() - 1;
-        int y = scanner.nextInt() - 1;
-        System.out.println(distance(adj, x, y));
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			adj[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < m; i++) {
+			int x, y;
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			adj[x - 1].add(y - 1);
+			adj[y - 1].add(x - 1);
+		}
+		int x = scanner.nextInt() - 1;
+		int y = scanner.nextInt() - 1;
+		System.out.println(distance(adj, x, y));
+	}
 }
 
--- Algorithms-on-Graph/week3/assignment/bipartite/Bipartite.java
+++ Algorithms-on-Graph/week3/assignment/bipartite/Bipartite.java
@@ -4,27 +4,41 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Bipartite {
-    private static int bipartite(ArrayList<Integer>[] adj) {
-        //write your code here
-        return -1;
-    }
+	private static int bipartite(ArrayList<Integer>[] adj) {
+		int[] partitions = new int[adj.length];
+		for(int i=0; i<adj.length; i++) partitions[i] = 0;
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(0);
+		partitions[0] = 1;
+		while(!q.isEmpty()){
+			int x = q.poll();
+			for(int y: adj[x]){
+				if(partitions[y]==partitions[x]) return 0;
+				if (partitions[y]==0) {
+					partitions[y] = -partitions[x];
+					q.add(y);
+				}
+			}
+		}
+		return 1;
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < m; i++) {
-            int x, y;
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-            adj[y - 1].add(x - 1);
-        }
-        System.out.println(bipartite(adj));
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			adj[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < m; i++) {
+			int x, y;
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			adj[x - 1].add(y - 1);
+			adj[y - 1].add(x - 1);
+		}
+		System.out.println(bipartite(adj));
+	}
 }
 

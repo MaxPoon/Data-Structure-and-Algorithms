74c61110e5042ec5275c117c0eb37370419fffe0
Max Poon <maxpanziyuan@gmail.com>
2017-01-06 11:28:09 +0800

    pass week2

--- Advanced-Algorithms-and-Complexity/week2/assignment/diet/Diet.java
+++ Advanced-Algorithms-and-Complexity/week2/assignment/diet/Diet.java
@@ -3,77 +3,378 @@ import java.util.*;
 
 public class Diet {
 
-    BufferedReader br;
-    PrintWriter out;
-    StringTokenizer st;
-    boolean eof;
+	BufferedReader br;
+	PrintWriter out;
+	StringTokenizer st;
+	boolean eof;
+	void solve() throws IOException {
+		int n = nextInt();
+		int m = nextInt();
+		double[][] A = new double[n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				A[i][j] = nextInt();
+			}
+		}
+		double[] b = new double[n];
+		for (int i = 0; i < n; i++) {
+			b[i] = nextInt();
+		}
+		double[] c = new double[m];
+		for (int i = 0; i < m; i++) {
+			c[i] = nextInt();
+		}
+		LinearEquationsSolver ls = new LinearEquationsSolver(A, b, c);
+		ls.print();
+	}
 
-    int solveDietProblem(int n, int m, double A[][], double[] b, double[] c, double[] x) {
-      Arrays.fill(x, 1);
-      // Write your code here
-      return 0;
-    }
+	Diet() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		solve();
+		out.close();
+	}
 
-    void solve() throws IOException {
-        int n = nextInt();
-        int m = nextInt();
-        double[][] A = new double[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                A[i][j] = nextInt();
-            }
-        }
-        double[] b = new double[n];
-        for (int i = 0; i < n; i++) {
-            b[i] = nextInt();
-        }
-        double[] c = new double[m];
-        for (int i = 0; i < m; i++) {
-            c[i] = nextInt();
-        }
-        double[] ansx = new double[m];
-        int anst = solveDietProblem(n, m, A, b, c, ansx);
-        if (anst == -1) {
-            out.printf("No solution\n");
+	public static void main(String[] args) throws IOException {
+		new Diet();
+	}
+
+	String nextToken() {
+		while (st == null || !st.hasMoreTokens()) {
+			try {
+				st = new StringTokenizer(br.readLine());
+			} catch (Exception e) {
+				eof = true;
+				return null;
+			}
+		}
+		return st.nextToken();
+	}
+
+	int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+}
+
+class LinearEquationsSolver {
+	double[][] A; //co-efficient matrix
+	double[] b; //output matrix for co-efficicnet
+	double[] c; //objective function matrix
+
+	int n;//no of input equations
+	int m; // no of variables
+	double maxValue = Double.NEGATIVE_INFINITY;
+	double[] result = null;
+	static double INFINITY = Math.pow(10,9);
+	private boolean bounded = true;
+
+	LinearEquationsSolver(double[][] A, double[] b, double[] c){
+		n = A.length;
+		m = c.length;
+		this.A = A;
+		this.b = b;
+		this.c = c;
+		//total no of inequalities = n +m + 1(for infinity)
+		int total = n + m +1;
+		compute(total);
+	}
+
+	public void print(){
+		if (maxValue == Double.NEGATIVE_INFINITY){
+			System.out.println("No solution");
+			return;
+		}
+		if (!bounded){
+			System.out.println("Infinity");
+			return;
+		}
+		System.out.println("Bounded solution");
+		for (int i=0; i< result.length; i++){
+			System.out.print(String.format("%.15f", result[i]) + " ");
+		}
+	}
+
+	private void compute(int total){
+		int[] arr = new int[total];
+		for (int i=0; i < arr.length; i++){
+			arr[i] = i;
+		}
+		genProcessCombinations(arr,total, m);
+	}
+
+	/**
+	 * Process subset of size
+	 * @param subset
+	 */
+	private void processSubset(Set<Integer> subset){
+		double[][] A = new double[m][m];
+		double[] b = new double[m];
+		updateMatrices(subset, A, b);
+		GaussianElimination gElim = new GaussianElimination(A, b);
+		if (!gElim.hasSolution){
+			return;
+		}
+		double[] temp_result = gElim.b;
+		if (!satisfiesAllInEq(temp_result)){
+			return;
+		}
+		double val = computeVal(temp_result);
+		if (val > maxValue){
+			maxValue = val;
+			result = temp_result;
+			if (subset.contains(n+m)){
+				bounded = false;
+			} else{
+				bounded = true;
+			}
+		}
+	}
+
+	/**
+	 * Verify result satisfies all the equations
+	 * @param result
+	 * @return
+	 */
+	private boolean satisfiesAllInEq(double[] result){
+		boolean satisfied = true;
+		//check to see if eq satisfies regular equations
+		for (int i=0; i < n; i++){
+			double inEqSum = 0;
+			for(int j= 0; j < m; j++){
+					inEqSum += result[j] * this.A[i][j];
+			}
+			if (inEqSum > b[i] + Math.pow(10, -3)){
+				//not satisfied
+				satisfied = false;
+				break;
+			}
+		}
+		//check to see if it satisfies constraints
+		for (int i=0 ; i < m; i++){
+			if (result[i] * -1 > Math.pow(10, -3)){
+				//not satisfied
+				satisfied = false;
+				break;
+			}
+
+		}
+		return  satisfied;
+
+	}
+
+	private double computeVal(double[] result){
+		double val = 0;
+		for (int i=0; i < result.length; i++){
+			val += result[i] * c[i];
+		}
+		return val;
+	}
+
+	/**
+	 * Update matrices
+	 */
+	private void updateMatrices(Set<Integer> set, double[][] A, double[] b){
+		int index = 0;
+		for (Integer val: set) {
+			if (val < n) {
+				A[index] = this.A[val];
+				b[index] = this.b[val];
+			}
+			else if (val < n+m){
+				int diff = val - n;
+				A[index] = new double[m];
+				A[index][diff] = -1;
+				b[index] = 0;
+			}else{
+				A[index] = new double[m];
+				Arrays.fill(A[index], 1);
+				b[index] = INFINITY;
+			}
+			index += 1;
+		}
+	}
+
+
+	/**
+	  arr[]  ---> Input Array
+	data[] ---> Temporary array to store current combination
+	start & end ---> Staring and Ending indexes in arr[]
+	index  ---> Current index in data[]
+	r ---> Size of a combination to be printed
+	 **/
+	void combinationUtil(int arr[], int n, int r, int index,
+								int data[], int i)
+	{
+		// Current combination is ready to be printed, print it
+		if (index == r)
+		{
+			Set<Integer> set = new HashSet<>();
+			for (int j=0; j<r; j++)
+				set.add(data[j]);
+			processSubset(set);
+			return;
+		}
+
+		// When no more elements are there to put in data[]
+		if (i >= n)
+			return;
+
+		// current is included, put next at next location
+		data[index] = arr[i];
+		combinationUtil(arr, n, r, index+1, data, i+1);
+
+		// current is excluded, replace it with next (Note that
+		// i+1 is passed, but index is not changed)
+		combinationUtil(arr, n, r, index, data, i+1);
+	}
+
+	/**
+	 *     The main function that prints all combinations of size r
+	 *      in arr[] of size n. This function mainly uses combinationUtil()
+	 */
+	 void genProcessCombinations(int arr[], int n, int r)
+	{
+		// A temporary array to store all combination one by one
+		int data[]=new int[r];
+		//Set<Set<Integer>> result = new HashSet<Set<Integer>>();
+		// Print all combination using temprary array 'data[]'
+		combinationUtil(arr, n, r, 0, data, 0);
+	}
+}
+
+class GaussianElimination {
+    private double[][] A; //refers to co-efficient matrix
+    double[] b; //refers to output matrix
+    boolean hasSolution = true;
+
+
+    GaussianElimination(double[][] A, double[] b){
+        if (A == null || b == null){
+            hasSolution = false;
             return;
         }
-        if (anst == 0) {
-            out.printf("Bounded solution\n");
-            for (int i = 0; i < m; i++) {
-                out.printf("%.18f%c", ansx[i], i + 1 == m ? '\n' : ' ');
-            }
+        if (A.length == 0 || b.length == 0){
+            hasSolution = false;
             return;
         }
-        if (anst == 1) {
-            out.printf("Infinity\n");
-            return;
-        }
+        copyMatrix(A);
+        copyMatrix(b);
+        rowReduce();
     }
 
-    Diet() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(System.out);
-        solve();
-        out.close();
-    }
+    private void rowReduce(){
+        int rowLength = A.length;
+        for (int row =0; row < rowLength; row++){
+            int rowPivot = getRowPivot(A, row);
+            if (rowPivot == -1){
+                hasSolution = false;
+                return;
+            }
+            if (rowPivot != row){
+                //swap rows
+                swapRowsInA(row, rowPivot);
+                swapIndexInb(row, rowPivot);
+            }
+            //pivot element is located in col <row> for current row < row>
+            //rescale to make pivot as 1
+            if (A[row][row] != 1){
+                //rescale entire row
+                rescalePivot(row);
+            }
+            //make col zero
+            for (int r = 0; r < rowLength; r++){
+                if (row == r){
+                    continue;
+                }
+                makeColZero(row, r);
 
-    public static void main(String[] args) throws IOException {
-        new Diet();
-    }
-
-    String nextToken() {
-        while (st == null || !st.hasMoreTokens()) {
-            try {
-                st = new StringTokenizer(br.readLine());
-            } catch (Exception e) {
-                eof = true;
-                return null;
             }
         }
-        return st.nextToken();
     }
 
-    int nextInt() throws IOException {
-        return Integer.parseInt(nextToken());
+    /**
+     * Print result of b
+     */
+    public void printResult(){
+        if (hasSolution) {
+            for (int row = 0; row < b.length; row++) {
+                System.out.print(b[row] + " ");
+            }
+        }
     }
+
+    /**
+     * Make col entries as zero for the pivot row of A and update b
+     * @param current_row pi
+     * @param row
+     */
+    private void makeColZero(int current_row, int row){
+        double scale_factor = A[row][current_row];
+        for (int col = 0; col < A[0].length; col++){
+            A[row][col] = A[row][col] - scale_factor * A[current_row][col];
+        }
+        b[row] = b[row] - scale_factor * b[current_row];
+    }
+
+    private void rescalePivot(int row){
+        double scale_factor = A[row][row];
+        for (int col =0 ; col < A[0].length; col++){
+            A[row][col] = A[row][col]/scale_factor;
+        }
+        b[row] = b[row] / scale_factor;
+    }
+
+    /**
+     * Swap rows i and j of A
+     */
+    private void swapRowsInA(int i, int j){
+        double[] temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    /**
+     * Swap values in ith and jth index of b
+     */
+    private void swapIndexInb(int i, int j){
+        double temp = b[i];
+        b[i] = b[j];
+        b[j] = temp;
+    }
+
+    private int getRowPivot(double[][] matrix, int row){
+        //select first non zero entry in left most column
+        for (int r = row; r < matrix.length; r++){
+            if (matrix[r][row] != 0){
+                return r;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Copy input A to have a local copy < avoid modifying client</>
+     * @param matrix
+     */
+    private void copyMatrix(double[][] matrix){
+        A = new double[matrix.length][matrix[0].length];
+        for (int i=0; i < matrix.length; i++){
+            for (int j=0; j< matrix[0].length; j++){
+                this.A[i][j] = matrix[i][j];
+            }
+        }
+    }
+
+    /**
+     * Copy input B to have a local copy < avoid modifying client</>
+     * @param matrix
+     */
+    private void copyMatrix(double[] matrix){
+        b = new double[matrix.length];
+        for (int i=0; i< matrix.length; i++){
+                b[i] = matrix[i];
+        }
+
+    }
+
 }
--- Advanced-Algorithms-and-Complexity/week2/assignment/diet/diet.py
+++ Advanced-Algorithms-and-Complexity/week2/assignment/diet/diet.py
@@ -1,24 +1,402 @@
 # python3
+class NumberTypeclass:
+	def zero(self): return 0
+	def one(self): return 1
+	def positive(self,x): return x > 0
+	def iszero(self,x): return x == 0
+	def nonnegative(self,x): return self.positive(x) or self.iszero(x)
+	def coerce(self, x): return x
+	def coerce_vec(self, x): return [self.coerce(xi) for xi in x]
+	def coerce_mtx(self, x): return [self.coerce_vec(xi) for xi in x]
+	
+
+class RealFiniteTolerance(NumberTypeclass):
+	def __init__(self, eps=1e-6):
+		super(RealFiniteTolerance, self).__init__()
+		self.eps = eps
+		assert eps >= 0
+		
+	def zero(self): return 0.0
+	def one(self): return 1.0
+	def iszero(self,x): return abs(x) < self.eps
+	def coerce(self, x): return float(x)
+
+def _subtract_scaled_row(row1, row2, k, numclass):
+	"""row1 -= k*row2"""
+	if numclass.iszero(k): return
+	for i, row2_i in enumerate(row2):
+		row1[i] -= k*row2_i
+
+RESOLUTION_NO = "no"
+RESOLUTION_SOLVED = "solved"
+RESOLUTION_UNBOUNDED = "unbounded"
+RESOLUTION_INCOMPATIBLE = "incompatible"
+
+		
+class SimplexSolver:
+	def __init__(self, a, b, c, basis, numclass, clean_c_row=False ):
+		assert len(a) == len(b) #number of rows of A must be the same as number of elements of b
+		for aj in a:
+			assert len(aj) == len(c) #each row of A must have the same len as c
+			
+		self.numclass = numclass
+		self.a = a
+		self.b = b
+		self.c = c
+		self.basis = basis
+		self.n = len(c)
+		self.m = len(b)
+		self.resolution = RESOLUTION_NO
+		if clean_c_row: self._diagonalize_c_row()
+		self._validate_diagonzlized()
+		
+	def _diagonalize_c_row(self):
+		#diagonalize it (it has nonzero values now)
+		c = self.c
+		for j, i in enumerate(self.basis):
+			if not self.numclass.iszero(c[i]):
+				_subtract_scaled_row( c, self.a[j], c[i], self.numclass)
+				assert self.numclass.iszero( c[i] )
+				c[i] = self.numclass.zero()
+
+		
+	def vertex(self):
+		v = [self.numclass.zero()] * self.n
+		for i ,val in zip(self.basis, self.b):
+			v[i] = val
+		return v
+	def _validate_diagonzlized(self):
+		for i in self.basis:
+			assert self.numclass.iszero(self.c[i])
+
+		for j, a_j in enumerate(self.a):
+			for j1, i in enumerate(self.basis):
+				if j1 == j:
+					assert self.numclass.iszero( a_j[i] - self.numclass.one() )
+				else:
+					if not self.numclass.iszero( a_j[i] ): raise AssertionError("A: column {i} row {j} must be 0".format(**locals()))
+		for bi in self.b:
+			assert self.numclass.nonnegative(bi)
+
+	def _find_leading_column(self):
+		imin = min(range(self.n), key=lambda i: self.c[i])
+		if self.numclass.nonnegative(self.c[imin]):
+			return None
+		else:
+			return imin
+						
+	def step(self):
+		#determine leading column
+		#index of a minimal value
+		i_lead = self._find_leading_column()
+
+		if i_lead is None:
+			self.resolution = RESOLUTION_SOLVED
+			return False
+
+		assert i_lead not in self.basis
+
+		#now find the leading row
+		best_ratio = None
+		best_row = None
+
+		for j, b_j in enumerate(self.b):
+			a_ji = self.a[j][i_lead]
+
+			#treat special case when A contains zeros
+			if self.numclass.iszero(a_ji):
+				continue
+
+			#zeros in B are special. They mean coinciding vertices.
+			#Treat such ratios as positive if A element is positive.
+			if self.numclass.iszero(b_j):
+				#print("b_j is zero:", b_j)
+				if not self.numclass.positive(a_ji):
+					continue
+			ratio = b_j / a_ji
+			if not self.numclass.nonnegative(ratio): continue
+
+			if best_ratio is None or ratio < best_ratio:
+				best_ratio = ratio
+				best_row = j
+		if best_row is None:
+			self.resolution = RESOLUTION_UNBOUNDED
+			return False
+		#print("### col:",i_lead, "row:", best_row, "ratio", best_ratio)
+		#best row and column defined
+		self._diagonalize_by_row_col( best_row, i_lead )
+		#Update basis
+		self.basis[best_row] = i_lead
+		self._validate_diagonzlized()
+		return True
+	
+	def _diagonalize_by_row_col( self, j, i ):
+		"""Diagonalize problem relatively to given row and column
+		j - row
+		j - col
+		"""
+		a_ji = self.a[j][i]
+		assert not self.numclass.iszero(a_ji)
+
+		
+		#normalie j'th row
+		self.b[j] /= a_ji
+		aj = self.a[j]
+		for i1 in range(self.n):
+			if i1 != i:
+				aj[i1] /= a_ji
+			else:
+				aj[i1] = self.numclass.one()
+		
+		
+		#clear other rows
+		_subtract_scaled_row( self.c, aj, self.c[i], self.numclass)
+		self.c[i] = self.numclass.zero()
+
+		for j1, a_j1 in enumerate(self.a):
+			if j1 == j: continue
+			k = a_j1[i]
+			_subtract_scaled_row( a_j1, aj, k, self.numclass)
+			assert self.numclass.iszero(a_j1[i]) 
+			a_j1[i] = self.numclass.zero() #enforce zero
+			self.b[j1] -= self.b[j] * k
+			
+
+	def _show_row(self, first, row):
+		print( str(first) + '\t|' + "\t".join(map(str, row)))
+		
+	def show(self):
+		print( "Resolution:", self.resolution)
+		print( "Basis:", ", ".join('x{}'.format(i) for i in self.basis))
+		self._show_row('', ('x{}'.format(i) for i in range(self.n)))
+		self._show_row(' C', self.c)
+		for j in range(self.m):
+			self._show_row(self.b[j], self.a[j])
+			
+def simplex_canonical( a, b, c, basis, num, verbose=False, do_coerce = True):
+	"""Simplex method in canonical form, when initial basis is fully known"""
+	if do_coerce:
+		a = num.coerce_mtx(a)
+		b = num.coerce_vec(b)
+		c = num.coerce_vec(c)
+		
+	solver = SimplexSolver(a, b, c, basis, numclass=num, clean_c_row = True)
+	if verbose:
+		print("############ Regular simplex:#############")
+		solver.show()
+	while solver.resolution == RESOLUTION_NO:
+		solver.step()
+		if verbose:
+			print("############ Regular step:#############")
+			solver.show()
+
+	return solver.resolution, solver.vertex()
+
+def simplex_canonical_m( a, b, c, basis, num, verbose=False, do_coerce = True):
+	"""Simplex method in canonical form, when initial basis is not fully known.
+	Some or all elements of 'basis' can be None
+	"""
+	#apply artificial basis method.
+	if do_coerce:
+		a = num.coerce_mtx(a)
+		b = num.coerce_vec(b)
+		c = num.coerce_vec(c)
+	n_artificial = sum( int(bi is None) for bi in basis )
+	n = len(c)
+	if n_artificial == 0:
+		return simplex_canonical(a,b,c,basis, num, verbose=verbose, do_coerce=False)
+	
+	#Expand the problem with artificial variables
+	zeros = [num.zero()]*n_artificial
+	a = [ a_j+zeros for a_j in a ]
+	
+	i_next = n #next artificial variable
+	m_basis = basis[:]
+	for j, bi in enumerate(basis):
+		if bi is None:
+			a[j][i_next] = num.one()
+			m_basis[j] = i_next
+			i_next += 1
+	assert i_next == n + n_artificial
+	
+	#modified C vector for M-problem.
+	cm = [num.zero()]*n+[num.one()]*n_artificial
+
+	#Now solve M-problem step by step, until all artificial variables not in the basis
+	m_solver = SimplexSolver(a, b, cm, m_basis, num, clean_c_row=True)
+	real_vertex_reached = False
+	if verbose:
+		print("########### M-problem: #############")
+		m_solver.show()
+	while m_solver.resolution == RESOLUTION_NO:
+		m_solver.step()
+		if verbose:
+			print("########### M-step: #############")
+			m_solver.show()
+			
+		if all( bi < n for bi in m_solver.basis):
+			#m-problem solved!
+			if verbose:
+				print("### Real vertex reached")
+			real_vertex_reached = True
+			break
+		
+	if not real_vertex_reached:
+		#M-problem solved, but no real vertex.
+		if verbose:
+			print("### Empty simplex")
+		return RESOLUTION_INCOMPATIBLE, None
+	
+	#truncate m-problem back
+	a = [a_row[:n] for a_row in m_solver.a]
+	
+	return simplex_canonical(a, m_solver.b, c, m_solver.basis, num = num, verbose=verbose, do_coerce=False)
+
+def linsolve( objective,
+			  ineq_left=(), ineq_right=(),
+			  eq_left=(), eq_right=(),
+			  nonneg_variables = (),
+			  num=RealFiniteTolerance(),
+			  verbose=False, do_coerce=True):
+	""" Solve arbitrary linear programming problem:
+	Minimize linear function Cx -> min under set of conditions:
+		 Ax <= B
+		 A'x == B'
+	Arguments:
+	  ineq_left : MxN matrix A
+	  ineq_right: M-vector B
+	  eq_left: M'xN matrix A'
+	  eq_right: M'-vector B'
+	  nonneg_variables: list of variables (their 0-based indices) that are >= 0
+	  num: instance of NumberTypeclass, defining numeric implementation to be used. Default is RealFiniteTolerance().
+	  do_coerce: if True, all provided values are converted by the 'coerce' method of the typeclass before solving the problem. Added for testing convenience.
+	  verbose: if True, then solver prints solution steps to the console. Added for debug.
+	Returns tuple: (resulution, x).
+	Where resolution is one of RESOLUTION_SOLVED, RESOLUTION_INCOMPATIBLE, RESOLUTION_UNBOUNDED
+	If resolution is RESOLUTION_SOLVED, then x contains solution vector (list). Othervise, value of x is not defined.
+	"""
+	nonneg_variables = set(nonneg_variables)
+	n = len(objective)
+	assert all( len(aj) == n for aj in ineq_left )
+	assert all( len(aj) == n for aj in eq_left )
+
+	#we will add several artificial variables: negative parts of variables, inequality-to-equality artificial variables
+	#This stores index of the next fgree variable to add
+	next_variable = n
+
+	#first, create variables to map nonpositive variables
+	#  x -> x1 - x2
+	#  after substitution, x1 gets the same index as X, x2 gets new index
+
+	#map variable index -> variable index, maps x1 to x2.
+	negative_part2positive_part = {} 
+
+	for var in range(n):
+		if var not in nonneg_variables:
+			var_neg = next_variable
+			next_variable += 1
+			negative_part2positive_part[var_neg] = var
+
+	n_nonneg = next_variable
+	if verbose:
+		print ("Created", n_nonneg-n, "new varables to get rid of variables with unknown sign")
+
+	def positivise_row( row ):
+		#map row of A pr C matrix to positive variables
+		return [ row[i] if i not in negative_part2positive_part else -row[negative_part2positive_part[i]]
+				 for i in range(n_nonneg) ]
+
+	#Get rid of nonpositive variablesby substitution
+	c_nonneg = positivise_row(objective)
+	ineq_left_nonneg = list(map(positivise_row, ineq_left))
+	eq_left_nonneg = list(map(positivise_row, eq_left))
+	
+	def negate_row( row ): return [-ri for ri in row]
+
+	#Number of artificial Z variables is the same as number of inequalities
+	num_inequalities = len(ineq_left)
+	assert len(ineq_right) == num_inequalities
+
+	a_extended = []
+	b_extended = []
+	basis = []
+	
+	#Get rid of inequalities
+	for a_row, bi in zip(ineq_left_nonneg, ineq_right):
+		# Inequality (Ai, x) <= bi
+		a_row_extended = a_row + [num.zero()]*num_inequalities
+		artificial_var = next_variable
+		next_variable += 1
+		a_row_extended[artificial_var] = num.one()
+		#changed Ax <= b
+		# to Ax + z <= b
+
+		#Now ensure that b >= 0
+		if not num.nonnegative( bi ):
+			bi = -bi
+			a_row_extended = negate_row(a_row_extended)
+			#Since this row was negated, this artificial variable can't be added to the basis.
+			basis.append(None) 
+		else:
+			#if the variable is not negated, then it can be added to the basis.
+			basis.append(artificial_var)
+		a_extended.append( a_row_extended )
+		b_extended.append( bi )
+		
+	#And copy equalities, as-is, extending with zeros.
+	for a_row, bi in zip(eq_left_nonneg, eq_right):
+		if not num.nonnegative( bi ):
+			bi = -bi
+			a_row = negate_row(a_row)
+		a_extended.append( a_row + [num.zero()]*num_inequalities )
+		b_extended.append( bi )
+		basis.append(None)
+
+	#Phew! DOne!
+	#Now a_extended, b_extended, c_nonneg describe the problem.
+	#basis stores indices of varaibles that are already diagonalized.
+	#Solve it!
+
+	resolution, solution = simplex_canonical_m(a_extended,
+											   b_extended,
+											   c_nonneg+[num.zero()]*num_inequalities,
+											   basis,
+											   num=num,
+											   verbose=verbose,
+											   do_coerce = do_coerce)
+
+	if resolution == RESOLUTION_SOLVED:
+		#Remove artificial variables
+		orig_solution = solution[:n]
+		
+		#then fold back positive-negative pairs
+		for negative_var, positive_var in negative_part2positive_part.items():
+			orig_solution[positive_var] -= solution[negative_var]
+
+		return resolution, orig_solution
+	else:
+		return resolution, solution
+
+	
+# python3
 from sys import stdin
-  
-def solve_diet_problem(n, m, A, b, c):  
-  # Write your code here
-  return [0, [0] * m]
 
 n, m = list(map(int, stdin.readline().split()))
 A = []
+for i in range(m):
+	A.append([0]*i+[-1]+[0]*(m-i-1))
 for i in range(n):
-  A += [list(map(int, stdin.readline().split()))]
+	A += [list(map(int, stdin.readline().split()))]
 b = list(map(int, stdin.readline().split()))
 c = list(map(int, stdin.readline().split()))
+c = list(map(lambda x:-x, c))
+b = [0]*m + b
+resolution, sol = linsolve( c, ineq_left = A, ineq_right = b)
 
-anst, ansx = solve_diet_problem(n, m, A, b, c)
-
-if anst == -1:
-  print("No solution")
-if anst == 0:  
-  print("Bounded solution")
-  print(' '.join(list(map(lambda x : '%.18f' % x, ansx))))
-if anst == 1:
-  print("Infinity")
-    
+if resolution == "incompatible":
+	print("No solution")
+if resolution == "solved":  
+	print("Bounded solution")
+	print(' '.join(list(map(lambda x : '%.6f' % x, sol))))
+if resolution == "unbounded":
+	print("Infinity")

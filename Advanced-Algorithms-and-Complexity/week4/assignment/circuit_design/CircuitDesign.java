8cdc13c8cd30a481116e110fda1dff553678c4c9
Max Poon <maxpanziyuan@gmail.com>
2017-01-08 11:50:32 +0800

    finish course

--- Advanced-Algorithms-and-Complexity/week4/assignment/circuit_design/CircuitDesign.java
+++ Advanced-Algorithms-and-Complexity/week4/assignment/circuit_design/CircuitDesign.java
@@ -1,146 +1,224 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class CircuitDesign {
-    private final InputReader reader;
-    private final OutputWriter writer;
+	private final InputReader reader;
+	private final OutputWriter writer;
 
-    public CircuitDesign(InputReader reader, OutputWriter writer) {
-        this.reader = reader;
-        this.writer = writer;
-    }
+	public CircuitDesign(InputReader reader, OutputWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
+	}
 
-    public static void main(String[] args) {
-        InputReader reader = new InputReader(System.in);
-        OutputWriter writer = new OutputWriter(System.out);
-        new CircuitDesign(reader, writer).run();
-        writer.writer.flush();
-    }
+	public static void main(String[] args) {
+      new Thread(null, new Runnable() {
+			public void run() {
+				InputReader reader = new InputReader(System.in);
+				OutputWriter writer = new OutputWriter(System.out);
+				new CircuitDesign(reader, writer).run();
+				writer.writer.flush();
+			}
+		}, "1", 1 << 26).start();
+   }
 
-    class Clause {
-        int firstVar;
-        int secondVar;
-    }
+	class Clause {
+		int firstVar;
+		int secondVar;
+	}
 
-    class TwoSatisfiability {
-        int numVars;
-        Clause[] clauses;
+	class TwoSatisfiability {
+		int numVars;
+		Clause[] clauses;
+		ArrayList<HashSet<Integer>> sccs = new ArrayList<HashSet<Integer>>();
 
-        TwoSatisfiability(int n, int m) {
-            numVars = n;
-            clauses = new Clause[m];
-            for (int i = 0; i < m; ++i) {
-                clauses[i] = new Clause();
-            }
-        }
+		TwoSatisfiability(int n, int m) {
+			numVars = n;
+			clauses = new Clause[m];
+			for (int i = 0; i < m; ++i) {
+				clauses[i] = new Clause();
+			}
 
-        boolean isSatisfiable(int[] result) {
-            // This solution tries all possible 2^n variable assignments.
-            // It is too slow to pass the problem.
-            // Implement a more efficient algorithm here.
-            for (int mask = 0; mask < (1 << numVars); ++mask) {
-                for (int i = 0; i < numVars; ++i) {
-                    result[i] = (mask >> i) & 1;
-                }
+		}
 
-                boolean formulaIsSatisfied = true;
+		void setup(){
+			ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[2*numVars];
+			ArrayList<Integer>[] adjR = (ArrayList<Integer>[])new ArrayList[2*numVars];
+			for (int i = 0; i < 2*numVars; i++) {
+				adj[i] = new ArrayList<Integer>();
+				adjR[i] = new ArrayList<Integer>();
+			}
+			constructImplicationGraph(adj, adjR);
+			LinkedList<Integer> order = new LinkedList<Integer>();
+			boolean[] visited = new boolean[2*numVars];
+			for(int i=0; i<2*numVars; i++){
+				if(!visited[i]){
+					visited[i] = true;
+					dfs1(adjR, visited, i, order);
+					order.add(i);
+				}
+			}
+			Collections.reverse(order);
+			for(int i=0; i<adj.length; i++) visited[i] = false;
+			
+			for(int x: order){
+				if(!visited[x]){
+					HashSet<Integer> scc = new HashSet<Integer>();
+					visited[x] = true;
+					dfs2(adj, visited, x, scc);
+					sccs.add(scc);
+				}
+			}
+		}
 
-                for (Clause clause: clauses) {
-                    boolean clauseIsSatisfied = false;
-                    if ((result[Math.abs(clause.firstVar) - 1] == 1) == (clause.firstVar < 0)) {
-                        clauseIsSatisfied = true;
-                    }
-                    if ((result[Math.abs(clause.secondVar) - 1] == 1) == (clause.secondVar < 0)) {
-                        clauseIsSatisfied = true;
-                    }
-                    if (!clauseIsSatisfied) {
-                        formulaIsSatisfied = false;
-                        break;
-                    }
-                }
+		boolean isSatisfiable(int[] result) {
+			for(HashSet<Integer> scc: sccs){
+				for(int x: scc){
+					if(scc.contains(x+numVars) || scc.contains(x-numVars)) return false;
+				}
+			}
+			for(int i=0; i<result.length; i++) result[i] = -1;
+			for(HashSet<Integer> scc: sccs){
+				for(int i: scc){
+					int x;
+					boolean isNegative;
+					if(i>=numVars){
+						x = i-numVars;
+						isNegative = true;
+					}else{
+						x = i;
+						isNegative = false;
+					}
+					if(result[x]==-1){
+						if(isNegative)	result[x] = 1;
+						else result[x] = 0;
+					}
+				}
+			}
+			return true;
+		}
 
-                if (formulaIsSatisfied) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
+		void constructImplicationGraph(ArrayList<Integer>[] adj, ArrayList<Integer>[] adjR){
 
-    public void run() {
-        int n = reader.nextInt();
-        int m = reader.nextInt();
+			for(Clause clause: clauses){
+				int x, y, notX, notY;
+				
+				if(clause.firstVar>0) {
+					x = clause.firstVar-1;
+					notX = x + numVars;
+				}else{
+					x = -clause.firstVar - 1 + numVars;
+					notX = x - numVars;
+				}
 
-        TwoSatisfiability twoSat = new TwoSatisfiability(n, m);
-        for (int i = 0; i < m; ++i) {
-            twoSat.clauses[i].firstVar = reader.nextInt();
-            twoSat.clauses[i].secondVar = reader.nextInt();
-        }
+				if(clause.secondVar>0) {
+					y = clause.secondVar-1;
+					notY = y + numVars;
+				}else {
+					y = -clause.secondVar - 1 + numVars;
+					notY = y - numVars;
+				}
 
-        int result[] = new int[n];
-        if (twoSat.isSatisfiable(result)) {
-            writer.printf("SATISFIABLE\n");
-            for (int i = 1; i <= n; ++i) {
-                if (result[i-1] == 1) {
-                    writer.printf("%d", -i);
-                } else {
-                    writer.printf("%d", i);
-                }
-                if (i < n) {
-                    writer.printf(" ");
-                } else {
-                    writer.printf("\n");
-                }
-            }
-        } else {
-            writer.printf("UNSATISFIABLE\n");
-        }
-    }
+				adj[notX].add(y);
+				adj[notY].add(x);
+				adjR[y].add(notX);
+				adjR[x].add(notY);
+			}
+		}
 
-    static class InputReader {
-        public BufferedReader reader;
-        public StringTokenizer tokenizer;
+		void dfs1(ArrayList<Integer>[] adj, boolean[] visited, int x, LinkedList<Integer> order){
+			for(int nextX: adj[x]){
+				if(!visited[nextX]){
+					visited[nextX] = true;
+					dfs1(adj, visited, nextX, order);
+					order.add(nextX);
+				}
+			}
+		}
 
-        public InputReader(InputStream stream) {
-            reader = new BufferedReader(new InputStreamReader(stream), 32768);
-            tokenizer = null;
-        }
+		void dfs2(ArrayList<Integer>[] adj, boolean[] visited, int x, HashSet<Integer> scc){
+			scc.add(x);
+			for(int nextX: adj[x]){
+				if(!visited[nextX]){
+					visited[nextX] = true;
+					dfs2(adj, visited, nextX, scc);
+				}
+			}
+		}
+	}
 
-        public String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return tokenizer.nextToken();
-        }
+	public void run() {
+		int n = reader.nextInt();
+		int m = reader.nextInt();
 
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
+		TwoSatisfiability twoSat = new TwoSatisfiability(n, m);
+		for (int i = 0; i < m; ++i) {
+			twoSat.clauses[i].firstVar = reader.nextInt();
+			twoSat.clauses[i].secondVar = reader.nextInt();
+		}
+		twoSat.setup();
 
-        public double nextDouble() {
-            return Double.parseDouble(next());
-        }
+		int result[] = new int[n];
+		if (twoSat.isSatisfiable(result)) {
+			writer.printf("SATISFIABLE\n");
+			for (int i = 1; i <= n; ++i) {
+				if (result[i-1] == 1) {
+					writer.printf("%d", -i);
+				} else {
+					writer.printf("%d", i);
+				}
+				if (i < n) {
+					writer.printf(" ");
+				} else {
+					writer.printf("\n");
+				}
+			}
+		} else {
+			writer.printf("UNSATISFIABLE\n");
+		}
+	}
 
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-    }
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
 
-    static class OutputWriter {
-        public PrintWriter writer;
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
 
-        OutputWriter(OutputStream stream) {
-            writer = new PrintWriter(stream);
-        }
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
 
-        public void printf(String format, Object... args) {
-            writer.print(String.format(Locale.ENGLISH, format, args));
-        }
-    }
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	static class OutputWriter {
+		public PrintWriter writer;
+
+		OutputWriter(OutputStream stream) {
+			writer = new PrintWriter(stream);
+		}
+
+		public void printf(String format, Object... args) {
+			writer.print(String.format(Locale.ENGLISH, format, args));
+		}
+	}
 }
--- Advanced-Algorithms-and-Complexity/week4/assignment/plan_party/PlanParty.java
+++ Advanced-Algorithms-and-Complexity/week4/assignment/plan_party/PlanParty.java
@@ -2,78 +2,87 @@ import java.io.*;
 import java.util.*;
 
 class Vertex {
-    Vertex() {
-        this.weight = 0;
-        this.children = new ArrayList<Integer>();
-    }
-
-    int weight;
-    ArrayList<Integer> children;
+	Vertex() {
+		this.weight = 0;
+		this.children = new ArrayList<Integer>();
+	}
+	int maxFun;
+	int weight;
+	ArrayList<Integer> children;
 }
 
-class starter_java {
-    static Vertex[] ReadTree() throws IOException {
-        InputStreamReader input_stream = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input_stream);
-        StreamTokenizer tokenizer = new StreamTokenizer(reader);
+class PlanParty {
+	static Vertex[] ReadTree() throws IOException {
+		InputStreamReader input_stream = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input_stream);
+		StreamTokenizer tokenizer = new StreamTokenizer(reader);
 
-        tokenizer.nextToken();
-        int vertices_count = (int) tokenizer.nval;
+		tokenizer.nextToken();
+		int vertices_count = (int) tokenizer.nval;
 
-        Vertex[] tree = new Vertex[vertices_count];
+		Vertex[] tree = new Vertex[vertices_count];
 
-        for (int i = 0; i < vertices_count; ++i) {
-            tree[i] = new Vertex();
-            tokenizer.nextToken();
-            tree[i].weight = (int) tokenizer.nval;
-        }
+		for (int i = 0; i < vertices_count; ++i) {
+			tree[i] = new Vertex();
+			tokenizer.nextToken();
+			tree[i].weight = (int) tokenizer.nval;
+		}
 
-        for (int i = 1; i < vertices_count; ++i) {
-            tokenizer.nextToken();
-            int from = (int) tokenizer.nval;
-            tokenizer.nextToken();
-            int to = (int) tokenizer.nval;
-            tree[from - 1].children.add(to - 1);
-            tree[to - 1].children.add(from - 1);
-        }
+		for (int i = 1; i < vertices_count; ++i) {
+			tokenizer.nextToken();
+			int from = (int) tokenizer.nval;
+			tokenizer.nextToken();
+			int to = (int) tokenizer.nval;
+			tree[from - 1].children.add(to - 1);
+			tree[to - 1].children.add(from - 1);
+		}
 
-        return tree;
-    }
+		return tree;
+	}
 
-    static void dfs(Vertex[] tree, int vertex, int parent) {
-        for (int child : tree[vertex].children)
-            if (child != parent)
-                dfs(tree, child, vertex);
+	static void dfs(Vertex[] tree, int vertex, int parent) {
+		for (int child : tree[vertex].children)
+			if (child != parent)
+				dfs(tree, child, vertex);
+		int maxFun1 = tree[vertex].weight;
+		int maxFun2 = 0;
+		for(int child: tree[vertex].children){
+			if(child!=parent){
+				maxFun2 += tree[child].maxFun;
+				for(int grandchild: tree[child].children){
+					if(grandchild!= child && grandchild!=parent){
+						maxFun1 += tree[grandchild].maxFun;
+					}
+				}
+			}
+		}
+		tree[vertex].maxFun = Math.max(maxFun1, maxFun2);
+	}
 
-        // This is a template function for processing a tree using depth-first search.
-        // Write your code here.
-        // You may need to add more parameters to this function for child processing.
-    }
+	static int MaxWeightIndependentTreeSubset(Vertex[] tree) {
+		int size = tree.length;
+		if (size == 0)
+			return 0;
+		dfs(tree, 0, -1);
+		// You must decide what to return.
+		return tree[0].maxFun;
+	}
 
-    static int MaxWeightIndependentTreeSubset(Vertex[] tree) {
-        int size = tree.length;
-        if (size == 0)
-            return 0;
-        dfs(tree, 0, -1);
-        // You must decide what to return.
-        return 0;
-    }
+	public static void main(String[] args) throws IOException {
+	  // This is to avoid stack overflow issues
+	  new Thread(null, new Runnable() {
+					public void run() {
+						try {
+							new PlanParty().run();
+						} catch(IOException e) {
+						}
+					}
+				}, "1", 1 << 26).start();
+	}
 
-    public static void main(String[] args) throws IOException {
-      // This is to avoid stack overflow issues
-      new Thread(null, new Runnable() {
-                    public void run() {
-                        try {
-                            new PlanParty().run();
-                        } catch(IOException e) {
-                        }
-                    }
-                }, "1", 1 << 26).start();
-    }
-
-    public void run() throws IOException {
-        Vertex[] tree = ReadTree();
-        int weight = MaxWeightIndependentTreeSubset(tree);
-        System.out.println(weight);
-    }
+	public void run() throws IOException {
+		Vertex[] tree = ReadTree();
+		int weight = MaxWeightIndependentTreeSubset(tree);
+		System.out.println(weight);
+	}
 }
